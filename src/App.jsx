5ef1258555577c15b2c3f00f80649cd5c18aeafc
Rishabh-tiwari0 <htrishabh.tiwari@gmail.com>
2024-04-17 09:57:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="qr-container">
+      <img src="image-qr-code.png" alt="qr code" />
+      <h3>Improve your front-end skills by building projects</h3>
+      <p>
+        scan the QR code to visit front end mentor and take your coding skills
+        to next level
+      </p>
+    </div>
+  );
+}
+
+export default App;
